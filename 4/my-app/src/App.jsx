6424803049fa38c0739806eaf78f976af6d1aeb5
yuki3738 <y.minamiya@gmail.com>
2023-06-01 00:42:41 +0900

    4-4まで

--- 4/my-app/src/App.jsx
+++ 4/my-app/src/App.jsx
@@ -1,8 +1,18 @@
 export const App = () => {
+  const onClickButton = () => {
+    alert();
+  }
+
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20px"
+  };
+
   return (
     <div>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </div>
   )
 }
