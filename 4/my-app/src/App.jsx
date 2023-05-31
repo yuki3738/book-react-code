afb7db6f81a6eadd57e3164b28f7d707180a1b95
yuki3738 <y.minamiya@gmail.com>
2023-06-01 01:57:05 +0900

    4-6まで

--- 4/my-app/src/App.jsx
+++ 4/my-app/src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { ColoredMessage } from './components/ColoredMessage';
 
 export const App = () => {
+  const [num, setNum] = useState(0);
   const onClickButton = () => {
-    alert();
+    // setNum(prev + 1);
+    setNum((prev) => prev + 1);
   }
 
   return (
@@ -11,6 +14,7 @@ export const App = () => {
       <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
       <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </div>
   )
 }
